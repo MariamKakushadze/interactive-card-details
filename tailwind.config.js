4e687424957ffd4057cdc4d91253c2d88169f047
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2022-12-02 22:15:00 +0400

    added tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    letterSpacing: {
+      wider: "1.1px",
+      widest: "2.2px",
+      xl: "3.42px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
